82283f578714d902cc755d608b527aa227f85da1
AnzorKarto <beit06@mail.ru>
2023-03-10 20:59:12 +0300

    Min settings for server-app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const morgan = require('morgan')
+const cors = require('cors')
+const socket = require('socket.io')
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log('База подклбчена')
+  })
+  .catch((e) => {
+    console.log(e.toString())
+  })
+
+const server = app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err.toString())
+  }
+  console.log(`Сервер запущен на порту ${PORT}`)
+})
