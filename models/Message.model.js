16da8211da0d9b9fdc7318b903fb69454d0904c4
AnzorKarto <beit06@mail.ru>
2023-03-12 00:05:34 +0300

    did messages controller

--- models/Message.model.js
+++ models/Message.model.js
@@ -1,1 +1,22 @@
-//Message model
+const mongoose = require('mongoose')
+
+const messageSchema = mongoose.Schema(
+  {
+    message: {
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+    users: Array,
+    sender: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const Messages = mongoose.model('Messages', messageSchema)
+module.exports = Messages
