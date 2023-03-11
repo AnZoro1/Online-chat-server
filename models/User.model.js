22aec2d5bb507a772d8a5c4530728d747ae98159
AnzorKarto <beit06@mail.ru>
2023-03-11 21:32:30 +0300

    did user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,33 @@
-// User model
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 15,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: '  ',
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
